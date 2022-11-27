8852f38ae40867a3b324f1c5d8e3a291c7964791
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-11-27 17:36:54 -0500

    added mobile view

---
+++ public/assets/src/detectMob.js
@@ -0,0 +1,12 @@
+function detectMob() {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i,
+  ];
+  return toMatch.some((toMatchItem) => navigator.userAgent.match(toMatchItem));
+}
